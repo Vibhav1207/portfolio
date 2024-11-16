0cdaa4df93afffd70a5e48d344c41f7c90697056
Vibahv Patel <77201529+Vibhav1207@users.noreply.github.com>
2024-11-16 22:15:00 +0530

    Update project-card.tsx

--- components/sub/project-card.tsx
+++ components/sub/project-card.tsx
@@ -5,57 +5,34 @@ type ProjectCardProps = {
   src: string;
   title: string;
   description: string;
-  githubLink: string; 
-  liveDemoLink: string;
+  link: string;
 };
 
 export const ProjectCard = ({
   src,
   title,
   description,
-  githubLink,
-  liveDemoLink,
+  link,
 }: ProjectCardProps) => {
   return (
-    <div className="relative overflow-hidden rounded-lg shadow-lg border border-[#2A0E61]">
-      <div className="absolute inset-0 border-4 border-transparent rounded-lg neon-border"></div>
-      <Link
-        href={liveDemoLink}
-        target="_blank"
-        rel="noreferrer noopener"
-        className="relative z-10"
-      >
-        <Image
-          src={src}
-          alt={title}
-          width={1000}
-          height={1000}
-          className="w-full object-contain"
-        />
-      </Link>
+    <Link
+      href={link}
+      target="_blank"
+      rel="noreferrer noopener"
+      className="relative overflow-hidden rounded-lg shadow-lg border border-[#2A0E61]"
+    >
+      <Image
+        src={src}
+        alt={title}
+        width={1000}
+        height={1000}
+        className="w-full object-contain"
+      />
 
-      <div className="relative p-4 z-10">
+      <div className="relative p-4">
         <h1 className="text-2xl font-semibold text-white">{title}</h1>
         <p className="mt-2 text-gray-300">{description}</p>
-        <div className="flex justify-between mt-4">
-          <Link
-            href={githubLink}
-            target="_blank"
-            rel="noreferrer noopener"
-            className="bg-gray-800 text-white py-2 px-4 rounded hover:bg-gray-700"
-          >
-            GitHub
-          </Link>
-          <Link
-            href={liveDemoLink}
-            target="_blank"
-            rel="noreferrer noopener"
-            className="bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-400"
-          >
-            Live Demo
-          </Link>
-        </div>
       </div>
-    </div>
+    </Link>
   );
 };
